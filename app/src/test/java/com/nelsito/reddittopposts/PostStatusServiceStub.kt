3ee04174f5bc2586b564e00e563a7c670daa1b1f
Nelson Puglisi <nelson.puglisi@olx.com>
2020-01-29 17:58:02 -0300

    LoadPostsShould.return_posts_with_read_status: GREEN

--- app/src/test/java/com/nelsito/reddittopposts/PostStatusServiceStub.kt
+++ app/src/test/java/com/nelsito/reddittopposts/PostStatusServiceStub.kt
@@ -3,8 +3,8 @@ package com.nelsito.reddittopposts
 import com.nelsito.reddittopposts.domain.PostStatusService
 import com.nelsito.reddittopposts.domain.RedditPost
 
-class PostStatusServiceStub(postStatus: Map<String, Boolean>) : PostStatusService {
+class PostStatusServiceStub(private val postStatus: Map<String, Boolean>) : PostStatusService {
     override fun applyStatus(posts: List<RedditPost>): List<RedditPost> {
-        return posts
+        return posts.map { it.copy(read = postStatus[it.id] ?: false) }
     }
 }
