76593ff2a18714f076544b0f4457260d63773235
Nelson Puglisi <nelson.puglisi@olx.com>
2020-01-29 19:27:58 -0300

    FAB removed

--- app/src/main/java/com/nelsito/reddittopposts/ItemListActivity.kt
+++ app/src/main/java/com/nelsito/reddittopposts/ItemListActivity.kt
@@ -48,11 +48,6 @@ class ItemListActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         toolbar.title = title
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-
         if (item_detail_container != null) {
             // The detail container view will be present only in the
             // large-screen layouts (res/values-w900dp).
